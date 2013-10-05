6da7ebc9d9f4dd99a8a49813cb82397847bbf167
Nicolas Dumont <nic_dumont@hotmail.com>
2013-10-05 10:09:29 +0200

    depot initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SiteLyly::Application.config.secret_key_base = 'fbce180324591e9fa0d4da7df528502e86ed8183bd3c25f1bf9f4d2eccf0b129f9703ea6899aca72b80a7e5d65e0c30aed11b3842e013787a5d78c34252a49e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SiteLyly::Application.config.session_store :cookie_store, key: '_site_lyly_session'
